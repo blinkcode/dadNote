d23046cd9e23c71f734c5ff414743c20ea9b605a
lihongliang <lihongliang@inspur.com>
2020-08-21 01:08:43 +0800

    删除无用代码

---
+++ src/app/setting/setting.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-setting',
+  templateUrl: './setting.page.html',
+  styleUrls: ['./setting.page.scss'],
+})
+export class SettingPage implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+}
