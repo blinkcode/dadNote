a41c1d03ace7f63ad5ed44c8358f09ce594f5533
lihongliang <lihongliang@inspur.com>
2020-09-12 22:26:13 +0800

    格式化

--- src/app/common/events/event.service.ts
+++ src/app/common/events/event.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EventService {
-  private sub: Subject<any> = new Subject();
-  getSub() {
-    return this.sub;
-  }
-  publish(data: any) {
-    this.sub.next(data);
-  }
+    private sub: Subject<any> = new Subject();
+    getSub() {
+        return this.sub;
+    }
+    publish(data: any) {
+        this.sub.next(data);
+    }
 }
